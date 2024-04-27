94acf6221d8af1b6930dafef028240871903796d
pedroteixeira2002 <8200489@estg.ipp.pt>
2024-04-27 14:58:46 +0100

    problemas

--- Milestone1/models/entity.js
+++ Milestone1/models/entity.js
@@ -1,14 +1,15 @@
 var mongoose = require('mongoose');
 
 var EntitySchema = new mongoose.Schema({
-    // In future try to add an image field to the entity schema
-    name: { type: String, required: true },
-    description: { type: String, required: true },
+    name: {type: String, required: true},
+    contact: {type: String, required: true},
+    email: {type: String, required: true},
+    description: {type: String, required: true},
     address: {
-        countryCode: { type: String, required: true },
-        postalCode: { type: String, required: true },
-        addressLocality: { type: String, required: true },
-        streetAddress: { type: String, required: true },
+        countryCode: {type: String, required: true},
+        postalCode: {type: String, required: true},
+        addressLocality: {type: String, required: true},
+        streetAddress: {type: String, required: true},
     },
 });
 
