6f2dfd6d13205652fc7133d557ec88bd5e4a4174
Miguel Ribeiro <8170538@estg.ipp.pt>
2024-04-27 15:47:13 +0100

    ESTÁ UM DESASTRE

--- Milestone1/models/entity.js
+++ Milestone1/models/entity.js
@@ -1,15 +1,6 @@
 var mongoose = require('mongoose');
 
 var EntitySchema = new mongoose.Schema({
-<<<<<<< HEAD
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    contact: { type: String, required: true },
-    address: { type: String, required: true },
-    status: { type: String, enum: ['Pendente', 'Aprovado', 'Rejeitado'], default: 'Pendente'},
-    donationsReceived:[{type: mongoose.Schema.Types.ObjectId, ref: 'Donation'}],
-    //typeEntity: { type: String, enum: ['Person', 'Organization'], required: true },
-=======
     // In future try to add an image field to the entity schema
     name: {type: String, required: true},
     contact: {type: String, required: true},
@@ -21,7 +12,6 @@ var EntitySchema = new mongoose.Schema({
         addressLocality: {type: String, required: true},
         streetAddress: {type: String, required: true},
     },
->>>>>>> origin/pedro
 });
 
 module.exports = mongoose.model('Entity', EntitySchema);
