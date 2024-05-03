269b92483e1fe42a8a87d0fb6a17842745a87f5c
Miguel Ribeiro <8170538@estg.ipp.pt>
2024-05-03 22:29:48 +0100

    Commit Final

--- Milestone1/routes/dashboard.js
+++ Milestone1/routes/dashboard.js
@@ -1,7 +1,8 @@
 var express = require("express");
 var router = express.Router();
 var dashboardController = require("../controllers/dashboardController.js");
+var authentication = require("../controllers/authController.js");
 
-router.get("/", dashboardController.getDashboard);
+router.get("/", authentication.verifyLoginUser, dashboardController.getDashboard);
 
 module.exports = router;
