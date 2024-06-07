3cb0820b4980889203e43ccca5f94c6604e8792a
Miguel Ribeiro <8170538@estg.ipp.pt>
2024-06-07 19:04:36 +0100

    Fixing routes

--- Milestone1/routes/donators.js
+++ Milestone1/routes/donators.js
@@ -6,16 +6,16 @@ const authController = require('../controllers/authController.js');
 // Get all donators
 router.get('/', authController.verifyLoginUser, donatorController.showAll);
 // Get one donator by id
-router.get('/show/:id', donatorController.show, authController.verifyLoginUser);
+router.get('/show/:id', authController.verifyLoginUser, donatorController.show);
 // Form to create a donator
-router.get('/create', donatorController.formCreate, authController.verifyLoginUser);
+router.get('/create', authController.verifyLoginUser, donatorController.formCreate);
 // Save donator
-router.post('/create', donatorController.create, authController.verifyLoginUser);
+router.post('/create', authController.verifyLoginUser, donatorController.create);
 // Form to edit a donator
-router.get('/edit/:id', donatorController.formEdit, authController.verifyLoginUser);
+router.get('/edit/:id', authController.verifyLoginUser, donatorController.formEdit);
 // Save edited donator
-router.post('/edit/:id', donatorController.edit, authController.verifyLoginUser);
+router.post('/edit/:id', authController.verifyLoginUser, donatorController.edit);
 // Delete donator
-router.get('/delete/:id', donatorController.delete, authController.verifyLoginUser);
+router.get('/delete/:id', authController.verifyLoginUser, donatorController.delete);
 
 module.exports = router;
--- Milestone1/routes/entities.js
+++ Milestone1/routes/entities.js
@@ -7,16 +7,16 @@ var authentication = require("../controllers/authController.js");
 // Get all entities
 router.get('/', authentication.verifyLoginUser, entityController.showAll);
 // Get one entity by id
-router.get('/show/:id', entityController.show, authentication.verifyLoginUser);
+router.get('/show/:id', authentication.verifyLoginUser, entityController.show);
 // Form to create an entity
-router.get('/create', entityController.formCreate, authentication.verifyLoginUser);
+router.get('/create', authentication.verifyLoginUser, entityController.formCreate);
 // Save entity
-router.post('/create', entityController.create, authentication.verifyLoginUser);
+router.post('/create', authentication.verifyLoginUser, entityController.create);
 // Form to edit an entity
-router.get('/edit/:id', entityController.formUpdate, authentication.verifyLoginUser);
+router.get('/edit/:id', authentication.verifyLoginUser, entityController.formUpdate);
 // Save edited entity
-router.post('/edit/:id', entityController.update, authentication.verifyLoginUser);
+router.post('/edit/:id', authentication.verifyLoginUser, entityController.update);
 // Delete entity
-router.get('/delete/:id', entityController.delete, authentication.verifyLoginUser);
+router.get('/delete/:id', authentication.verifyLoginUser, entityController.delete);
 
 module.exports = router;
